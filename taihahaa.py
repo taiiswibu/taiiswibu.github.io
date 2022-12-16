d9cb98c2d8ef54c25bfc8c18ecfb1fd8655448d7
taiiswibu <vovantai2k4@gmail.com>
2022-12-16 16:32:28 +0700

    bhasauh\

---
+++ taihahaa.py
@@ -0,0 +1,9 @@
+import random 
+name = 'vovantai36'
+matkhau = '123456789'
+for i in range (3):
+    a = input('tên đăng nhập:')
+    b = input('mật khẩu:')
+    if a == name and b ==  matkhau:
+        print('đăng nhập thành công')
+    break
